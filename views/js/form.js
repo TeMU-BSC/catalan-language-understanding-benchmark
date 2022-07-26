07e83f8c041e7239a74c2596697870272a800a87
Pau Figueras <pau@pswsm.cat>
2022-07-26 11:39:16 +0200

    Add mail and link validation

--- views/js/form.js
+++ views/js/form.js
@@ -1,4 +1,31 @@
-function submit_form(e) {
+function check_mail (mail) {
+    let mailError = false;
+    if (/[\w_]+\@\w\.\w{2,3}/.exec(mail) == null) {
+        $("#emailDiv").addClass("has-error");
+        mailError = true;
+    }
+    return mailError
+}
+
+function check_link (paperLink) {
+    let linkError = false;
+    if (/http[s]?:\/\/[\w-]+\.\w{2,3}[\/\w-]*\.?\w{0,4}/.exec(paperLink) == null) {
+        $("#emailDiv").addClass("has-error");
+        linkError = true;
+    }
+    return linkError
+}
+
+function check_form () {
+    const formData = new FormData($("#evaluation_form")[0]);
+    const mailErr = check_mail (formData.get("email"));
+    const linkErr = check_link (formData.get("paperLink"));
+    if (mailErr && linkErr) {
+        submit_form ()
+    }
+}
+
+function submit_form() {
     $.ajax({
         url: "http://localhost:3000/api/results",
         type: "POST",
@@ -7,10 +34,6 @@ function submit_form(e) {
         contentType: false,
         success: submit_success,
         error: submit_error
-    }).done((data) => {
-        if (!/[\w_]+\@\w\.\w{2,3}/.exec(data["email"])) {
-            $("#emailDiv").addClass("has-error");
-        }
     });
 }
 
