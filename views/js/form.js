8d6e6fbcd5c2c332e33c7d0010db07b5ea34cca7
Pau Figueras <pau@pswsm.cat>
2022-07-26 17:24:50 +0200

    All form fields validation

--- views/js/form.js
+++ views/js/form.js
@@ -1,20 +1,38 @@
 function checkMail () {
   const mail = $('#email').val()
   if (/[\w\-]+@\w+\.\w{2,3}/.exec(mail) == null) {
-    $('#emailDiv').addClass('has-error')
-    console.log(mail + ' is not valid.')
+    $('#emailDiv').addClass('has-error').append('<div class="help-block">Mail ' + mail + ' is not valid</div>')
   } else {
     $('#emailDiv').removeClass('has-error')
+    $('#emailDiv > div.help-block').remove()
   }
 }
 
 function checkLink () {
   const paperLink = $('#paperLink').val()
   if (paperLink !== '' && /http[s]?:\/\/[\w-]+\.\w{2,3}[/\w-]*\.?\w{0,4}/.exec(paperLink) == null) {
-    $('#paperLinkDiv').addClass('has-error')
-    console.log('Link ' + paperLink + ' is not valid.')
+    $('#paperLinkDiv').addClass('has-error').append('<div class="help-block">Link ' + paperLink + ' is not valid</div>')
   } else {
     $('#paperLinkDiv').removeClass('has-error')
+    $('#paperLinkDiv > div.help-block').remove()
+  }
+}
+
+function checkFile(sender) {
+  if (sender.path === '') {
+    $('#' + sender.id).parent().addClass('has-error').append('<div class="help-block">This name is not valid</div>')
+  } else {
+    $('#' + sender.id).parent().removeClass('has-error')
+    $('#' + sender.id + ' + div.help-block').remove()
+  }
+}
+
+function checkText(sender) {
+  if (sender.value === '') {
+    $('#' + sender.id).parent().addClass('has-error').append('<div class="help-block">This name is not valid</div>')
+  } else {
+    $('#' + sender.id).parent().removeClass('has-error')
+    $('#' + sender.id + ' + div.help-block').remove()
   }
 }
 
@@ -22,7 +40,8 @@ function submitForm (e) {
   let formData = new FormData($('#evaluation_form')[0])
   // let mailValid = checkMail(formData.get('email'))
   // let linkValid = checkLink(formData.get('paperLink'))
-  // if (mailValid && linkValid) {
+  if (!$('#evaluation_form div').hasClass('has-error')) {
+    $('#submit_button').val('Submit')
     $.ajax({
       url: 'http://localhost:3000/api/results',
       type: 'POST',
@@ -31,8 +50,13 @@ function submitForm (e) {
       contentType: false,
       success: submitSuccess,
       error: submitError
+    }).done(() => {
+      $('#evaluation_form').css('display', 'none').parent().append('<img src="../ok.png" alt="Evaluation sent successfully">')
+      $('#evaluation_form + img').css('filter', 'invert(100%)').css('text-align', 'center')
+      $('#evaluation_form + p').css('display', 'none')
+      $('#evaluation_form + h1').text('Thanks for submitting!')
     })
-  // }
+  }
 }
 
 function submitSuccess () {
@@ -40,5 +64,5 @@ function submitSuccess () {
 }
 
 function submitError (err) {
-  // console.error(err)
+  console.error(err)
 }
