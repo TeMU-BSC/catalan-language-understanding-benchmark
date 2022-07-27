35882083948561429bd48b8091d1b9f701939f45
Pau Figueras <pau@pswsm.cat>
2022-07-27 11:47:58 +0200

    Update script to log error

--- javascripts/mi_script.js
+++ javascripts/mi_script.js
@@ -1,67 +1,66 @@
-function obtainTables() {
-    $.ajax({
-        url: "http://bsclsaina01.bsc.es:3000/api/tables",
-        dataType: "json",
-        // contentType: "application/json",
-        success: tableSuccess,
-        error: tableError
+function obtainTables () {
+  $.ajax({
+    url: 'http://bsclsaina01.bsc.es:3000/api/tables',
+    dataType: 'json',
+    // contentType: "application/json",
+    success: tableSuccess,
+    error: tableError
+  })
+}
+
+function tableSuccess (d) {
+  console.log(d)
+  const arr = d.map((element) => {
+    return [element.email, element.modelName, element.researchGroup, element.paperLink, element.STS_ca.combined_score, element.POS.F1, element.VilaQuAD.total, element.ViquiQuAD.total, element.XQuAD_Ca.total, element.TeCla.Accuracy, element.TECa.Accuracy, element.AnCora_ca.F1]
+    // return {email: element.email}
+    // return {
+    // email: element.email,
+    // modelName: element.modelName,
+    // researchGroup: element.researchGroup,
+    // paperLink: element.paperLink,
+    // task1: element.task1,
+    // task2: element.task2,
+    // task3: element.task3,
+    // task4: element.task4,
+    // task5: element.task5
+    // }
+  })
+  const headers = ['Rank', 'Model', 'Group', 'Paper', 'STS', 'POS', 'VilaQuAD', 'ViquiQuAD', 'XQuAD', 'TECa', 'TeCla', 'AnCora']
+
+  let innerTable = '<table class="table performaceTable">'
+  innerTable += '<tr>'
+  $(headers).each(function (header) {
+    innerTable += '<th>' + headers[header]
+  })
+
+  $(arr).each(function (elem) {
+    innerTable += '<tr>'
+    $(arr[elem]).each(function (innerElem) {
+      switch (innerElem) {
+        case 0:
+          innerTable += '<td>' + (elem + 1)
+          break
+        case 3:
+          innerTable += '<td><a>' + arr[elem][innerElem]
+          break
+        case 2:
+          innerTable += '<td>' + arr[elem][innerElem] + '<br>' + arr[elem][0]
+          break
+        default:
+          innerTable += '<td>' + arr[elem][innerElem]
+      }
     })
+  })
+
+  $('#leaderboard').html(innerTable)
 }
 
-function tableSuccess(d) {
-    console.log(d);
-    let arr = d.map((element)=>{
-        return [element.email, element.modelName, element.researchGroup, element.paperLink, element.STS_ca.combined_score, element.POS.F1, element.VilaQuAD.total, element.ViquiQuAD.total, element.XQuAD_Ca.total, element.TeCla.Accuracy, element.TECa.Accuracy, element.AnCora_ca.F1];
-        // return {email: element.email}
-        // return {
-            // email: element.email,
-            // modelName: element.modelName,
-            // researchGroup: element.researchGroup,
-            // paperLink: element.paperLink,
-            // task1: element.task1,
-            // task2: element.task2,
-            // task3: element.task3,
-            // task4: element.task4,
-            // task5: element.task5
-        // }
-    });
-    const headers = ['Rank', 'Model', 'Group', 'Paper', 'STS', 'POS', 'VilaQuAD', 'ViquiQuAD', 'XQuAD', 'TECa', 'TeCla', 'AnCora'];
-
-    let innerTable = '<table class="table performaceTable">';
-    innerTable += '<tr>';
-    $(headers).each(function (header) {
-        innerTable += '<th>' + headers[header];
-    });
-
-    $(arr).each(function (elem) {
-        innerTable += '<tr>'
-        $(arr[elem]).each(function (innerElem) {
-            switch (innerElem) {
-                case 0:
-                    innerTable += '<td>' + (elem + 1);
-                    break;
-                case 3:
-                    innerTable += '<td><a>' + arr[elem][innerElem];
-                    break;
-                case 2:
-                    innerTable += '<td>' + arr[elem][innerElem] + '<br>' + arr[elem][0]
-                    break;
-                default:
-                    innerTable += '<td>' + arr[elem][innerElem];
-                }
-        });
-    });
-
-    $('#leaderboard').html(innerTable);
+function tableError (e) {
+  console.error(e)
 }
 
-function tableError(e) {
-    console.log(e);
-}
+$(document).ready(() => {
+  obtainTables()
 
-
-$(document).ready(()=>{
-    obtainTables();
-
-    // console.log(arr)
+  // console.log(arr)
 })
