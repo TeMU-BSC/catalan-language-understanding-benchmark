a9ee620381e551961fc4eab8defd0f51ceef3ee1
Pau Figueras <pau@pswsm.cat>
2022-09-22 10:44:54 +0200

    [WIP] Add cookie consent

--- views/js/cookieconsent-init.js
+++ views/js/cookieconsent-init.js
@@ -3,105 +3,78 @@ const cc = initCookieConsent();
 
 // Run plugin with your configuration
 cc.run({
-	current_lang: 'en',
-	autoclear_cookies: true, // Default: false
-	page_scripts: true, // Default: false
+	autorun = true,
+	current_lang = 'en',
+	delay = 0,
+	autoclear_cookies = true,
+	gui_options : {
+    consent_modal : {
+        layout : 'box',
+        position : 'bottom right',
+        transition : 'slide'
+    },
+    settings_modal : {
+        layout : 'box',
+		position : 'left',
+        transition : 'right'
+    }
+},
 
-	// mode: 'opt-in'                          // default: 'opt-in'; value: 'opt-in' or 'opt-out'
-	// delay: 0,                               // default: 0
-	// auto_language: null                     // default: null; could also be 'browser' or 'document'
-	// autorun: true,                          // default: true
-	// force_consent: false,                   // default: false
-	// hide_from_bots: false,                  // default: false
-	// remove_cookie_tables: false             // default: false
-	// cookie_name: 'cc_cookie',               // default: 'cc_cookie'
-	// cookie_expiration: 182,                 // default: 182 (days)
-	// cookie_necessary_only_expiration: 182   // default: disabled
-	// cookie_domain: location.hostname,       // default: current domain
-	// cookie_path: '/',                       // default: root
-	// cookie_same_site: 'Lax',                // default: 'Lax'
-	// use_rfc_cookie: false,                  // default: false
-	// revision: 0,                            // default: 0
+onAccept: function() {},
 
-	onFirstAction(user_preferences, cookie) {
-		// Callback triggered only once
-	},
+onChange: function(cookie) {},
 
-	onAccept(cookie) {
-		// ...
-	},
-
-	onChange(cookie, changed_preferences) {
-		// ...
-	},
-
-	languages: {
-		en: {
-			consent_modal: {
-				title: 'We use cookies!',
-				description: 'Hi, this website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Let me choose</button>',
-				primary_btn: {
-					text: 'Accept all',
-					role: 'accept_all', // 'accept_selected' or 'accept_all'
-				},
-				secondary_btn: {
-					text: 'Reject all',
-					role: 'accept_necessary', // 'settings' or 'accept_necessary'
-				},
+languages : {
+	'en' : {	
+		consent_modal : {
+			title :  "We use cookies",
+			description :  'Some description',
+			primary_btn: {
+				text: 'Accept all',
+				role: 'accept_all'
 			},
-			settings_modal: {
-				title: 'Cookie preferences',
-				save_settings_btn: 'Save settings',
-				accept_all_btn: 'Accept all',
-				reject_all_btn: 'Reject all',
-				close_btn_label: 'Close',
-				cookie_table_headers: [
-					{col1: 'Name'},
-					{col2: 'Domain'},
-					{col3: 'Expiration'},
-					{col4: 'Description'},
-				],
-				blocks: [
-					{
-						title: 'Cookie usage 📢',
-						description: 'We use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="#" class="cc-link">privacy policy</a>.',
-					}, {
-						title: 'Strictly necessary cookies',
-						description: 'These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly',
-						toggle: {
-							value: 'necessary',
-							enabled: true,
-							readonly: true, // Cookie categories with readonly=true are all treated as "necessary cookies"
-						},
-					}, {
-						title: 'Performance and Analytics cookies',
-						description: 'These cookies allow the website to remember the choices you have made in the past',
-						toggle: {
-							value: 'analytics', // Your cookie category
-							enabled: false,
-							readonly: false,
-						},
-						cookie_table: [ // List of all expected cookies
-							{
-								col1: '^_ga', // Match all cookies starting with "_ga"
-								col2: 'google.com',
-								col3: '2 years',
-								col4: 'description ...',
-								is_regex: true,
-							},
-							{
-								col1: '_gid',
-								col2: 'google.com',
-								col3: '1 day',
-								col4: 'description ...',
-							},
-						],
-					}, {
-						title: 'More information',
-						description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="#yourcontactpage">contact us</a>.',
-					},
-				],
-			},
+			secondary_btn: {
+				text : 'Accept necessary',
+				role : 'accept_necessary'
+			}
 		},
-	},
+		settings_modal : {
+			title : '<div>Cookie settings</div>',
+			save_settings_btn : "Save settings",
+			accept_all_btn : "Accept all",
+			cookie_table_headers : [
+				{col1: "Name" }, 
+				{col2: "Domain" }, 
+				{col3: "Expiration" }, 
+				{col4: "Description" },
+			],
+			blocks : [
+				{
+					title : "Cookie usage",
+					description: 'We use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details about cookies and how we use them, read the full <a href="../privacy-policy.html" class="cc-link">cookie policy</a>.'
+				},{
+					title : "Strictly necessary cookies",
+					description: 'These cookies are essential for the proper functioning of our website. Without these cookies, the website would not work properly.',
+					toggle : {
+						value : 'necessary_cookies',
+						enabled : true,
+						readonly: true							//cookie categories with readonly=true are all treated as "necessary cookies"
+					}
+				},{
+					title : "Preferences cookies",
+					description: 'These cookies allow the website to remember the choices you have made in the past.',
+					toggle : {
+						value : 'preferences_cookies',	//there are no default categories => you specify them
+						enabled : true,
+						readonly: false
+					}
+				},{
+					title : "More information",
+					description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="mailto:privacy@skylands.io">contact me</a>.',
+				}
+			]
+		}
+	}
+}
 });
+
