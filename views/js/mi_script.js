89dcb627eb308fb0f9632fe1e5e7035721b3612a
Pau Figueras <pau@pswsm.cat>
2022-10-05 13:43:06 +0200

    Add tabulator tables [WIP]

--- views/js/mi_script.js
+++ views/js/mi_script.js
@@ -10,72 +10,77 @@ function obtainTables () {
 }
 
 function tableSuccess (d) {
-  // console.log(d)
+  console.log(d)
 	// const sorted_objects = [...d].sort(function (a, b) {
 	// 	  return (a.scores.reduce((c, z) => c + z)/a.scores.length) - (b.scores.reduce((c, z) => c + z)/b.scores.length)
 	// }).reverse()
-	console.log(sorted_objects)
-  const arr = d.map((element) => {
-    return [
-      element.email,
-      element.modelName,
-      element.researchGroup,
-      element.paperLink,
-      Number(element.AnCora_ca.F1).toPrecision(4).toString(),
-      Number(element.POS.F1).toPrecision(4).toString(),
-      Number(element.STS_ca.combined_score).toPrecision(4).toString(),
-      Number(element.TeCla.Accuracy).toPrecision(4).toString(),
-      Number(element.TECa.Accuracy).toPrecision(4).toString(),
-      Number(element.CatalanQA_results.f1).toPrecision(4).toString() + '/' + Number(element.CatalanQA_results.exact).toPrecision(4).toString(),
-      Number(element.XQuAD_Ca.f1).toPrecision(4).toString() + '/' + Number(element.XQuAD_Ca.exact).toPrecision(4).toString()
-    ]
-  })
-  const headers = [
-    'Rank',
-    'Model',
-    'Group',
-    'Paper',
-    'NER (F1)',
-    'POS (F1)',
-    'STS-ca (Comb.)',
-    'TeCla (Acc.)',
-    'TE-Ca (Acc.)',
-    'CatalanQA (F1/EM)',
-    'XQuAD-ca (F1/EM)'
-  ]
-  const id_href = ['', '', '', '', 'ner', 'pos', 'sts', 'tecla', 'teca', 'catalanqa', 'xquad']
+	// console.log(sorted_objects)
+	let arr = d.map((element) => {
+		return [
+			element.email,
+			element.modelName,
+			element.researchGroup,
+			element.paperLink,
+			Number(element.AnCora_ca.F1).toPrecision(4).toString(),
+			Number(element.POS.F1).toPrecision(4).toString(),
+			Number(element.STS_ca.combined_score).toPrecision(4).toString(),
+			Number(element.TeCla.Accuracy).toPrecision(4).toString(),
+			Number(element.TECa.Accuracy).toPrecision(4).toString(),
+			Number(element.CatalanQA_results.f1).toPrecision(4).toString() + '/' + Number(element.CatalanQA_results.exact).toPrecision(4).toString(),
+			Number(element.XQuAD_Ca.f1).toPrecision(4).toString() + '/' + Number(element.XQuAD_Ca.exact).toPrecision(4).toString()
+		]
+	})
 
-  let innerTable = '<table class="table performanceTable">'
-  innerTable += '<tr>'
-  $(headers).each(function (header) {
-    // console.log(header)
-    if (header > 3) {
-      innerTable += '<th>' + '<a href="datasets.html#' + id_href[header].toLowerCase() + '">' + headers[header]
-    } else {
-      innerTable += '<th>' + headers[header]
-    }
-  })
+	console.log(arr)
+//	 const headers = [
+//	   // 'Rank',
+//	   'Model',
+//	   'Submitted By',
+//	   'Paper',
+//	   'NER (F1)',
+//		'POS (F1)',
+//	   'STS-ca (Comb.)',
+//	   'TeCla (Acc.)',
+//	   'TE-Ca (Acc.)',
+//	   'CatalanQA (F1/EM)',
+//	   'XQuAD-ca (F1/EM)'
+//	 ]
+//	 const id_href = ['', '', '', '', 'ner', 'pos', 'sts', 'tecla', 'teca', 'catalanqa', 'xquad']
 
-  $(arr).each(function (elem) {
-    innerTable += '<tr>'
-    $(arr[elem]).each(function (innerElem) {
-      switch (innerElem) {
-        case 0:
-          innerTable += '<td>' + (elem + 1)
-          break
-        case 3:
-          innerTable += '<td><a target="_blank" href=' + arr[elem][innerElem] + '><span class="material-symbols-outlined">open_in_new'
-          break
-        case 2:
-          innerTable += '<td>' + arr[elem][innerElem] // + '<br>' + arr[elem][0]
-          break
-        default:
-          innerTable += '<td>' + arr[elem][innerElem]
-      }
-    })
-  })
+//	 let innerTable = '<table class="table performanceTable">'
+//	 innerTable += '<tr>'
+//	 $(headers).each(function (header) {
+//	   // console.log(header)
+//	   if (header > 3) {
+//	     innerTable += '<th>' + '<a href="datasets.html#' + id_href[header].toLowerCase() + '">' + headers[header]
+//	   } else {
+//	     innerTable += '<th>' + headers[header]
+//	   }
+//	 })
 
-  $('#leaderboard').html(innerTable)
+//	 $(arr).each(function (elem) {
+//	   innerTable += '<tr>'
+//	   $(arr[elem]).each(function (innerElem) {
+//	     switch (innerElem) {
+//	       case 0:
+//	         innerTable += '<td>' + (elem + 1)
+//	         break
+//	       case 3:
+//	         innerTable += '<td><a target="_blank" href=' + arr[elem][innerElem] + '><span class="material-symbols-outlined">open_in_new'
+//	         break
+//	       case 2:
+//	         innerTable += '<td>' + arr[elem][innerElem] // + '<br>' + arr[elem][0]
+//	         break
+//	       default:
+//	         innerTable += '<td>' + arr[elem][innerElem]
+//	     }
+//	   })
+//	 })
+	let table = new Tabulator("#leaderboard", {
+		data:arr,
+		autoColumns:true,
+		responsiveLayout:"hide",
+	});
 }
 
 function tableError (e) {
@@ -83,7 +88,8 @@ function tableError (e) {
 }
 
 $(document).ready(() => {
-  obtainTables()
+	obtainTables();
+	// console.log(':', tableData)
 
   // console.log(arr)
 })
