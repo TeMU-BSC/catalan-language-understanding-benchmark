1438e7d026e780863de5f1f4c2812ccf3f81c80e
Pau Figueras <pau@pswsm.cat>
2022-12-14 13:05:51 +0100

    Update with toasts, better form and filepond

--- views/js/form.js
+++ views/js/form.js
@@ -89,8 +89,8 @@ function submitForm (e) {
 		}
 	}).showToast()
 	$.ajax({
-		// url: 'http://localhost:3000/api/results',
-		url: 'https://bscplantl01.bsc.es/evales/api/results',
+		url: 'http://localhost:3000/api/results',
+		// url: 'https://bscplantl01.bsc.es/evales/api/results',
 		type: 'POST',
 		data: formData,
 		processData: false,
